f33b2ff9158087f3ff4ac55240c714946fef4534
Torishnia <valeri.torishnya@gmail.com>
2023-01-27 19:13:56 +0300

    Create a new component Layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
+import Layout from './layout/Layout';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<App />);
+root.render(
+<React.StrictMode>
+  <Layout>
+    <App />
+  </Layout>
+</React.StrictMode>
+);
 
